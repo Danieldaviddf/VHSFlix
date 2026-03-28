4a955e0c229838cc9fe3b98d0a2ec244283ccd9f
Alexis Rodriguez <alexisrrh123@gmail.com>
2026-03-28 13:23:19 +0000

    definicion de colores del Navbar

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo1 from "./assets/logo1.png";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -7,10 +7,10 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <di>
+    <div>
     <Navbar/>
     <Row/>
-    </di>
+    </div>
   );
 }
 
