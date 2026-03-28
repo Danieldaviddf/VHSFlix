5b8371fcdb008e45180060734a0a4e45e7eab573
Alexis Rodriguez <alexisrrh123@gmail.com>
2026-03-28 21:05:27 +0100

    creacion del componente MovieCard

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import Navbar from '../components/Navbar';
+import Row from '../components/Row';
+import Carrousel from '../components/Carrousel'
+import Footer from '../components/Footer'
+import React, {useEffect, useState} from "react";
+import MovieCard from '../components/MovieCard';
 
 const Home = () => {
 
+
+  const [movies, setMovies] = useState([]);
+
+ 
+    async function peliculas() {
+        
+      try { let response = await fetch("https://archive.org/advancedsearch.php?q=mediatype:movies&fl[]=identifier,title&rows=10&output=json",{ method:"GET"});
+        const data = await response.json();
+          console.log(data)
+        setMovies(data.response.docs);
+      } 
+
+      catch (error) {
+      return  console.log("Error:", error);
+      }  }
+
+useEffect(()=>{
+     peliculas();
+},[])
+return(
+ <div>
+    <Navbar/>
+    <Carrousel />
+    <MovieCard movies={movies}/>
+    <Footer/>
+    </div>
+    
+)
 }
 
 export default Home;
