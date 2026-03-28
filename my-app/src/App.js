b10c7403ba7b24ade4274423d5e6d0367814ae94
Alexis Rodriguez <alexisrrh123@gmail.com>
2026-03-28 10:39:55 +0000

    creacion del nabvar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Row from './components/Row';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <di>
+    <Navbar/>
     <Row/>
+    </di>
   );
 }
 
